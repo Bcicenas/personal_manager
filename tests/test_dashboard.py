519c51f1f07053eb953d5a5b692abe3809cc6183
Benediktas <bcicenas@gmail.com>
2022-10-26 20:53:47 +0300

    dasboard unit test

--- tests/test_dashboard.py
+++ tests/test_dashboard.py
@@ -1,16 +1,14 @@
 import pytest
 
+def test_index(client, auth):
+	response = client.get('/', follow_redirects=True)
+	assert b"Log In" in response.data
+	assert b"Register" in response.data
 
-
-# def test_index(client, auth):
-# 	response = client.get('/')
-# 	assert b"Log In" in response.data
-# 	assert b"Register" in response.data
-
-# 	auth.login()
-# 	response = client.get('/')
-# 	assert b'Log Out' in response.data
-# 	assert b'test title' in response.data
-# 	assert b'by test on 2018-01-01' in response.data
-# 	assert b'test\nbody' in response.data
-# 	assert b'href="/1/update"' in response.data
+	auth.login()
+	response = client.get('/', follow_redirects=True)
+	assert b'Dashboard' in response.data
+	assert b'Shopping Lists' in response.data
+	assert b'test_primary' in response.data
+	assert b'item1' in response.data
+	assert b'task1' in response.data
