55494a79f7f5b21908c4a3e22636a19ed044e9ca
Benediktas <bcicenas@gmail.com>
2023-09-28 12:09:59 +0300

    Current months plans calendar

--- personal_manager/dashboard.py
+++ personal_manager/dashboard.py
@@ -2,10 +2,14 @@ from flask import (
 	Blueprint, flash, g, redirect, render_template, request, url_for, current_app
 )
 from werkzeug.exceptions import abort
-
+from flask import session
 from personal_manager.auth import login_required
-from . models import User, ShoppingList, Task
+from . models import User, ShoppingList, Task, Plan
 from . import db
+
+import calendar
+from .utils import CustomHTMLCal
+from datetime import datetime
 
 bp = Blueprint('dashboard', __name__)
 
@@ -13,15 +17,43 @@ bp = Blueprint('dashboard', __name__)
 def index():
 	if g.user is None:
 		return render_template('landing_page.html')
-		
-	# shopping lists
-	shopping_lists = db.session.execute(
-		db.select(ShoppingList).filter_by(user_id=g.user.id).order_by(ShoppingList.created_at.desc()).limit(10)
-	).fetchall()
+	
+	# calendar
+	if 'calendar-date' not in session:
+		session['calendar-date'] = datetime.now()
+	else:
+		if request.form.get('calendar-date') is not None:
+			session['calendar-date'] = request.form.get('calendar-date')
 
-	# tasks
-	tasks = db.session.execute(
-		db.select(Task).filter_by(user_id=g.user.id).order_by(Task.created_at.desc()).limit(10)
+	year = session['calendar-date'].year
+	month = session['calendar-date'].month
+	
+	# get month plans for calendar
+	# last day for query
+	day_range = calendar.monthrange(year, month)
+	start_day = datetime(year, month, 1)
+	end_day = datetime(year, month, day_range[1])
+	plans = db.session.execute(
+		db.select(Plan).filter(Plan.plan_date.between(start_day, end_day), Plan.user_id==g.user.id)
 	).fetchall()
+	
+	plan_data = {}
+	for i in range(day_range[1]):
+		plan_data [str(i + 1)] = []
 
-	return render_template('dashboard/index.html', shopping_lists=shopping_lists, tasks=tasks)
+	for plan in plans:
+		plan_data[str(plan[0].plan_date.day)].append('<a href="%s">%s<a>' % (url_for('plan.update', id=plan[0].id), plan[0].name))
+
+	this_month_calendar = CustomHTMLCal(day_data=plan_data).formatmonth(year, month)
+
+	# # shopping lists
+	# shopping_lists = db.session.execute(
+	# 	db.select(ShoppingList).filter_by(user_id=g.user.id).order_by(ShoppingList.created_at.desc()).limit(10)
+	# ).fetchall()
+
+	# # tasks
+	# tasks = db.session.execute(
+	# 	db.select(Task).filter_by(user_id=g.user.id).order_by(Task.created_at.desc()).limit(10)
+	# ).fetchall()
+
+	return render_template('dashboard/index.html', calendar=this_month_calendar)
--- personal_manager/utils.py
+++ personal_manager/utils.py
@@ -1,4 +1,25 @@
 from datetime import datetime
+import calendar
 
 def get_current_year():
 	return datetime.now().strftime('%Y')
+
+class CustomHTMLCal(calendar.HTMLCalendar):
+	# cssclasses = [style + " text-nowrap" for style in
+	# 			  calendar.HTMLCalendar.cssclasses]
+	# cssclass_month_head = "text-center month-head"
+	cssclass_month = "table month calendar-table"
+	# cssclass_year = "text-italic lead"
+	def __init__(self, day_data=None):
+		self.day_data = day_data
+		super().__init__()
+
+	def formatday(self, day, weekday):
+		"""
+		Return a day as a table cell.
+		"""
+		if day == 0:
+			# day outside month
+			return '<td class="%s">&nbsp;</td>' % self.cssclass_noday
+		else:
+			return '<td class="%s">%d %s</td>' % (self.cssclasses[weekday], day, ', '.join(self.day_data[str(day)]))
