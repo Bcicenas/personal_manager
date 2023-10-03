6c69ffca657c3fcd6720082e84206423a70c6d3b
Benediktas <bcicenas@gmail.com>
2023-10-03 22:07:21 +0300

    ajax fix

--- personal_manager/static/tasks_form.js
+++ personal_manager/static/tasks_form.js
@@ -18,10 +18,11 @@ $('.task-add-btn').click(function(){
 		selected_tasks.push(selected_task.val());
 		$('#task_list').append(
 			'<tr><td><input type="hidden" name="selected_tasks[][\'task_id\']" value="' + selected_task.val() + '" /><a href="/tasks/update/' + selected_task.val() + '">' + selected_task.text() + 
-			'<td><input type="hidden" name="selected_tasks[][\'start_time\']" value="' + selected_start_time.val() + '" />' + selected_start_time.text() +' h</td><td>' + getTaskDuration(selected_task.val()) + '</td>'+
+			'<td><input type="hidden" name="selected_tasks[][\'start_time\']" value="' + selected_start_time.val() + '" />' + selected_start_time.text() +' h</td><td class="selected-duration-' + selected_task.val() + '"></td>'+
 			'</a></td><td class="text-right"><input class="btn btn-danger task-remove-btn" type="button" value="' + btn_translation + '"></td></tr>'
 		);
 
+		getTaskDuration(selected_task.val());
 		$('.task-remove-btn').click(function(){ 
 			$(this).parent().parent().remove(); 
 			var selected_task_val = selected_task.val();
@@ -35,6 +36,6 @@ function getTaskDuration(task_id){
   var result = '0.0 h'
   $.get("/tasks/get_task_duration/" + task_id, function(data, status){
   	result = parseInt(data) / 60;
+  	$('.selected-duration-' + task_id).text(result + ' h');
   });
-  return result	
 }
